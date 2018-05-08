b46627493823385c5c0c9fac2c9ee14f15208832
splDead <AveSatan@6>
2018-05-08 18:13:57 +0300

    in progress #6 Поправил ресайз графика, тултипы, подписи по нижней оси

--- src/components/LinearChart.js
+++ src/components/LinearChart.js
@@ -4,11 +4,19 @@ import * as d3 from 'd3';
 import React from 'react';
 import type { StatisticsLogType } from '../types/types';
 
+import './LinearChart.css';
+
 type Props = {
     logs: Array<StatisticsLogType>
 };
 
 export class LinearChart extends React.Component<Props> {
+
+    refreshChart = () => {
+        d3.select('svg').remove();
+        this.showChart();
+    };
+
     showChart = () => {
         let group;
         const { logs } = this.props;
@@ -21,8 +29,16 @@ export class LinearChart extends React.Component<Props> {
             left : 50
         };
 
+        const addTooltipDiv = () => {
+            let tooltipDiv = d3.select('body').append('div')
+                .attr('class', 'tooltip')
+                .style('opacity', 0);
+
+            return tooltipDiv;
+        };
+
         const width = target.clientWidth - margin.left - margin.right;
-        const heigth = (target.clientWidth / 16) * 9 - margin.top - margin.bottom;
+        const height = 400 - margin.top - margin.bottom;
         const parseTime = d3.timeParse('%Y-%m-%d');
 
         const x = d3.scaleTime()
@@ -31,7 +47,7 @@ export class LinearChart extends React.Component<Props> {
 
         const y = d3.scaleLinear()
             .domain(d3.extent(logs.map(el => el.count)))
-            .range([heigth, 0]);
+            .range([height, 0]);
 
         const line = d3.line()
             .x(d => x(parseTime(d.date)) + 10)
@@ -39,7 +55,7 @@ export class LinearChart extends React.Component<Props> {
 
         group = d3.select(target)
             .append('svg')
-            .attr('height', heigth + margin.top + margin.bottom)
+            .attr('height', height + margin.top + margin.bottom + 40)
             .attr('width', width + margin.left + margin.right)
             .append('g')
             .attr('transform', `translate(${margin.left}, ${margin.top})`);
@@ -50,6 +66,8 @@ export class LinearChart extends React.Component<Props> {
             .style('stroke-width', 2)
             .style('fill', 'none');
 
+        let tooltipDiv = addTooltipDiv();
+
         group.selectAll('circle')
             .data(logs)
             .enter()
@@ -57,7 +75,28 @@ export class LinearChart extends React.Component<Props> {
             .attr('cx', d => x(parseTime(d.date)) + 10)
             .attr('cy', d => y(d.count))
             .attr('r', 4)
-            .style('fill', 'orange');
+            .attr('class', 'tooltip__circle')
+            .style('fill', 'orange')
+            .on('mouseover', d => {
+                tooltipDiv.transition()
+                    .duration(200)
+                    .style('opacity', .9);
+                tooltipDiv.html(`
+                        <div class='tooltip__title'>
+                            ${d.date}
+                        </div>
+                        <div class='tooltip__count'>
+                            Counts of logs = ${d.count}
+                        </div>
+                    `)
+                    .style('left', (d3.event.pageX - 150) + 'px')
+                    .style('top', (d3.event.pageY - 28) + 'px');
+            })
+            .on('mouseout', () => {
+                tooltipDiv.transition()
+                    .duration(500)
+                    .style('opacity', 0);
+            });
 
         group.append('g')            
             .call(
@@ -66,7 +105,7 @@ export class LinearChart extends React.Component<Props> {
                 .tickFormat(d3.timeFormat("%Y-%m-%d"))
             )
             .attr("class", "x-axis")
-            .attr('transform', `translate(10, ${heigth + 10})`);
+            .attr('transform', `translate(10, ${height + 10})`);
 
         d3.selectAll('g.x-axis g.tick')
             .append('line')
@@ -74,8 +113,11 @@ export class LinearChart extends React.Component<Props> {
             .attr('x1', 0)
             .attr('y1', 0)
             .attr('x2', 0)
-            .attr('y2', -heigth - 10)
-            .style('stroke', 'rgba(0, 0, 0, .05)')
+            .attr('y2', -height - 10)
+            .style('stroke', 'rgba(0, 0, 0, .05)');
+
+        d3.selectAll('g.x-axis g.tick text')
+            .attr('transform', `translate(-25, 20) rotate(-45)`);
 
         group.append('g')
             .call(
@@ -84,15 +126,19 @@ export class LinearChart extends React.Component<Props> {
                 .tickFormat(d3.format('.2s'))
             )
             .attr('transform', `translate(-10, 0)`);
-    }
+
+        d3.select(window)
+            .on("resize", () => {
+                this.refreshChart();
+            });
+    };
 
     componentDidMount() {
         this.showChart();        
     }
 
     componentDidUpdate() {
-        d3.select('svg').remove();
-        this.showChart();
+        this.refreshChart();
     }
 
     render() {
