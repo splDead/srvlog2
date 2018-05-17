4ce3ac9692f203d08cf29340689bd29ed6b4a8e4
splDead <AveSatan@6>
2018-05-17 15:49:01 +0300

    fixed #13

--- src/components/Search.js
+++ src/components/Search.js
@@ -13,19 +13,39 @@ const Search = ({ onSearch }: Props) => {
 
     const search = (e: SyntheticInputEvent<HTMLInputElement>) => {
         e.preventDefault();
-        onSearch(_inputValue);
+        if (_inputValue && _inputValue.value !== '') {
+            onSearch(_inputValue);
+        }
+    };
+
+    const clearSearchInput = (e: SyntheticEvent<>) => {
+        e.preventDefault();
         if (_inputValue) {
             _inputValue.value = '';
         }
     };
 
+    const handleClickSearchHint = (e: SyntheticEvent<HTMLElement>) => {
+        e.preventDefault();
+        if (_inputValue) {
+            _inputValue.value = e.currentTarget.getAttribute('data-pattern') || '';
+            _inputValue.focus();
+        }
+    };
+
     return (
-        <form className='input-group mb-3' onSubmit={search}>
-            <input type='text' className='form-control' ref={input => _inputValue = input} />
-            <div className='input-group-append'>
-                <button className='btn btn-primary special-z-index'>Search</button>
+        <React.Fragment>
+            <form className='input-group custom-form-container d-flex align-items-center' onSubmit={search}>
+                <input type='text' className='form-control' ref={input => _inputValue = input} />
+                <span className='clear-search' onClick={clearSearchInput}>×</span>
+                <div className='input-group-append'>
+                    <button className='btn btn-primary special-z-index'>Search</button>
+                </div>
+            </form>
+            <div className='mb-3'>
+                Patterns: <i className='pattern-hint' onClick={handleClickSearchHint} data-pattern='@program='>@program=value</i>
             </div>
-        </form>
+        </React.Fragment>
     )    
 };
 
